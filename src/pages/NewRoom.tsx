c968fdf154c122c137b2f12ca26adf258600732f
VictorOficial <victorrsoficial@gmail.com>
2021-06-24 09:58:22 -0300

    letmeask

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,36 @@
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
+    const { user } = useAuth();
+    const history = useHistory();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        const roomRef = database.ref('/rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`)
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -17,16 +42,18 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input
                             type="text"
                             placeholder="Nome da sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
                         </Button>
                     </form>
-                    <p>Quer entrar em uma sala existenter? <a href="#">Click aqui</a></p>
+                    <p>Quer entrar em uma sala existente? <Link to="/">Click aqui</Link></p>
                 </div>
             </main>
         </div>
